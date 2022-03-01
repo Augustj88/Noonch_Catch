8c9f133ec555cea134d9073fca7c611528b64218
paige1996 <guni33@naver.com>
2022-03-01 22:12:51 +0900

    Update room model, web socket connect disconnect, etc

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,9 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
-#
+from .models import Room
+from django.http import JsonResponse
+import json
 # from chat.models import UserGroup
-
 def index(request):
     # users = UserGroup.objects.all()
     # room = request.POST.get('roomname', '')
@@ -12,6 +14,14 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
-
 def ending(request):
     return render(request, 'chat/ending.html')
+
+def get_user(request):
+    roomname = json.loads(request.body.decode('utf-8'))
+    roomname = roomname['roomname']
+    rooms = Room.objects.filter(roomname=roomname)
+    users = {}
+    for i, room in enumerate(rooms):
+        users[i] = room.nickname
+    return JsonResponse(users)
